bbf691fb5d6df6ae15e35b6397841c9003569466
Buu Truong <btruong@hsph.harvard.edu>
2024-04-10 16:02:41 -0400

    update STAARpipeline_ensemble

--- R/noncoding.R
+++ R/noncoding.R
@@ -1,9 +1,11 @@
 noncoding <- function(chr,gene_name,genofile,obj_nullmodel,
-                      rare_maf_cutoff=0.01,rv_num_cutoff=2,
-                      QC_label="annotation/filter",variant_type=c("SNV","Indel","variant"),geno_missing_imputation=c("mean","minor"),
-                      Annotation_dir="annotation/info/FunctionalAnnotation",Annotation_name_catalog,
-                      Use_annotation_weights=c(TRUE,FALSE),Annotation_name=NULL,
-                      SPA_p_filter=FALSE,p_filter_cutoff=0.05,silent=FALSE){
+						rare_maf_cutoff=0.01,rv_num_cutoff=2,
+						QC_label="annotation/filter",
+						variant_type=c("SNV","Indel","variant"),
+						geno_missing_imputation=c("mean","minor"),
+						Annotation_dir="annotation/info/FunctionalAnnotation",Annotation_name_catalog,
+						Use_annotation_weights=c(TRUE,FALSE),Annotation_name=NULL,
+						SPA_p_filter=FALSE,p_filter_cutoff=0.05,silent=FALSE){
 
 	## evaluate choices
 	variant_type <- match.arg(variant_type)
